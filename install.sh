ffb17dc04d2422e66061fec9d01576a7a6997595
Anton Novojilov <andy@essentialkaos.com>
2017-08-09 00:37:47 +0200

    Improved install script

--- install.sh
+++ install.sh
@@ -2,6 +2,39 @@
 
 ################################################################################
 
+NORM=0
+BOLD=1
+UNLN=4
+RED=31
+GREEN=32
+YELLOW=33
+BLUE=34
+MAG=35
+CYAN=36
+GREY=37
+DARK=90
+
+CL_NORM="\e[0m"
+CL_BOLD="\e[0;${BOLD};49m"
+CL_UNLN="\e[0;${UNLN};49m"
+CL_RED="\e[0;${RED};49m"
+CL_GREEN="\e[0;${GREEN};49m"
+CL_YELLOW="\e[0;${YELLOW};49m"
+CL_BLUE="\e[0;${BLUE};49m"
+CL_MAG="\e[0;${MAG};49m"
+CL_CYAN="\e[0;${CYAN};49m"
+CL_GREY="\e[0;${GREY};49m"
+CL_DARK="\e[0;${DARK};49m"
+CL_BL_RED="\e[1;${RED};49m"
+CL_BL_GREEN="\e[1;${GREEN};49m"
+CL_BL_YELLOW="\e[1;${YELLOW};49m"
+CL_BL_BLUE="\e[1;${BLUE};49m"
+CL_BL_MAG="\e[1;${MAG};49m"
+CL_BL_CYAN="\e[1;${CYAN};49m"
+CL_BL_GREY="\e[1;${GREY};49m"
+
+################################################################################
+
 REPOSITORY="https://raw.githubusercontent.com/andyone/dotfiles/master/"
 
 ################################################################################
@@ -21,22 +54,22 @@ check() {
   local has_errors
 
   if [[ ! -e $HOME/.oh-my-zsh ]] ; then
-    echo "oh-my-zsh is reqired"
+    show "oh-my-zsh is reqired" $RED
     has_errors=true
   fi
 
   if ! isAppInstalled "zsh" ; then
-    echo "zsh is reqired"
+    show "zsh is reqired" $RED
     has_errors=true
   fi
 
   if ! isAppInstalled "tmux" ; then
-    echo "tmux is reqired"
+    show "tmux is reqired" $RED
     has_errors=true
   fi
 
   if ! isAppInstalled "git" ; then
-    echo "git is reqired"
+    show "git is reqired" $RED
     has_errors=true
   fi
 
@@ -57,25 +90,25 @@ doBackup() {
 
   tar cjf "$output" $file_list &> /dev/null
 
-  echo "Backup created as $output"
+  show "Backup created as $output" $GREY
 }
 
 doInstall() {
   local file
 
-  printf "Installing "
+  showm "Installing " $BOLD
 
   for file in ${theme[@]} ; do
     download "themes/$file" "$HOME/.oh-my-zsh"
-    printf "."
+    showm "•"
   done
 
   for file in ${files[@]} ; do
     download "$file" "$HOME"
-    printf "."
+    showm "•"
   done
 
-  echo " DONE"
+  show " DONE" $GREEN
 }
 
 getBackupFiles() {
@@ -104,6 +137,22 @@ isAppInstalled () {
   done
 
   return 0
+}
+
+show() {
+  if [[ -n "$2" && -z "$no_colors" ]] ; then
+    echo -e "\e[${2}m${1}\e[0m"
+  else
+    echo -e "$*"
+  fi
+}
+
+showm() {
+  if [[ -n "$2" && -z "$no_colors" ]] ; then
+    echo -e -n "\e[${2}m${1}\e[0m"
+  else
+    echo -e -n "$*"
+  fi
 }
 
 ################################################################################
